b25abf048ca677d6d72cef6b0b338c28e44d0ef8
cmc9595 <cmc9595@nate.com>
2022-09-26 16:33:06 +0900

    index page modal done

--- demo/src/main/java/com/example/demo/model/service/ProductServiceImpl.java
+++ demo/src/main/java/com/example/demo/model/service/ProductServiceImpl.java
@@ -2,7 +2,6 @@ package com.example.demo.model.service;
 
 import java.util.List;
 
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
