d19773feb66d92c43e83b313a314968616800875
bluesky0960 <bluesky096049@gmail.com>
2022-09-28 16:07:17 +0900

    벌레 업로드

--- recommendpage/src/RecommendItem.js
+++ recommendpage/src/RecommendItem.js
@@ -1,24 +1,41 @@
 import {Component} from 'react';
+import './component/base.css';
 
 class RecommendItem extends Component{
+    constructor(props){
+        super(props);
+        // this.handleMouseHover = this.handleMouseHover.bind(this);
+        
+        this.state ={
+            isHovering: false
+        };
+            
+    }
+
+    handleMouseHover = (e) => {
+        this.setState({
+            isHovering:!this.state.isHovering
+        });
+        
+        if(this.state.isHovering){
+            
+        }else if(!this.state.isHovering){
+
+        }
+    }
 
     render(){
         return(
-            <>
-                <div class="lists">
-                <dl class="prod_info">
-                    <dt><i class="ico ico_gy bz type26"></i><span>AI 방역로봇</span></dt>
+            <div className="lists" onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover}>
+                <dl className="prod_info">
+                    <dt>
+                        <img className='ico' src={require('./res/icons/ai/airCleanerAI.png')} width='70px' height='70px' alt='' />
+                        <span>AI 방역로봇</span>
+                    </dt>
                     <dd>일생을 감염의 위험과 함께하는 엔데믹의 시대에
                         일상의 안전을 지키는 AI 로봇 기반의 새로운 방역 패러다임</dd>
                 </dl>
-                <span class="chk_wrap">
-                    <input type="checkbox" name="ipt_chk" id="ipt_chk1" value="B201" data-prod-id="B201" data-prod-title="Biz인터넷"/>
-                    <label for="ipt_chk1" onclick="location.href='https:/enterprise.kt.com/pd/P_PD_AI_RB_005.do';">
-                        <span class="hide">Biz인터넷 상품 선택</span>
-                    </label>
-                </span>
-                </div>
-            </>
+            </div>
         )
     }
 }
