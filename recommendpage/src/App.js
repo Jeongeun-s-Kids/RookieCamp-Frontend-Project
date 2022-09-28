fef61314cf4b7d622ba4a147fd6d3a0aa6e4a919
bluesky0960 <bluesky096049@gmail.com>
2022-09-28 09:14:33 +0900

    Recommand page html -> react로 변경

---
+++ recommendpage/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import RecommendItem from './RecommendItem';
+
+function App() {
+  return (
+    <div className="App">
+        <RecommendItem />
+    </div>
+  );
+}
+
+export default App;
