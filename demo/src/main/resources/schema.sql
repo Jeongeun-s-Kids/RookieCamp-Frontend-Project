d74935d21046f55ab19a3008b7d67efd84b149cb
bluesky0960 <bluesky096049@gmail.com>
2022-09-29 16:48:00 +0900

    PJE-8 sql문 변경, 서버통신 params 변경

--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -1,11 +1,16 @@
--- product Table
+-- 첫 테이블 생성시에는 주석 유지
+-- 이미 테이블이 있는 경우 주석 풀고 사용
 drop table product if exists;
+drop table satisfaction if exists;
+
 create table product(
     seq int not null auto_increment primary key,
     name varchar(30) not null,
     type varchar(30) not null,
     category varchar(30) not null,
-    link varchar(300) not null);
+    link varchar(300) not null,
+    icon varchar(100) not null,
+    content varchar(500) not null);
 
 -- satisfaction Table
 drop table satisfaction if exists;
